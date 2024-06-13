46a0d2ca9919a620ec77bb4c7cd877b5eab26a01
Jack Hunt <jackmileshunt@gmail.com>
2024-06-13 23:30:14 +0100

    Create simpleDigitsClassifier.m

---
+++ networks/simpleDigitsClassifier.m
@@ -0,0 +1,27 @@
+function layers = simpleDigitsClassifier()
+%SIMPLEDIGITSCLASSIFIER A simple MNIST convnet taken from the docs.
+    layers = [
+        % Block 1
+        imageInputLayer([28 28 1])
+        convolution2dLayer(3, 8, Padding="same")
+        batchNormalizationLayer
+        reluLayer
+        averagePooling2dLayer(2, Stride=2)
+        % Block 2
+        convolution2dLayer(3, 16, Padding="same")
+        batchNormalizationLayer
+        reluLayer
+        averagePooling2dLayer(2, Stride=2)
+        % Block 3
+        convolution2dLayer(3, 32, Padding="same")
+        batchNormalizationLayer
+        reluLayer
+        % Block 4
+        convolution2dLayer(3, 32, Padding="same")
+        batchNormalizationLayer
+        reluLayer
+        % Block 5
+        fullyConnectedLayer(10)
+    ];
+end
+

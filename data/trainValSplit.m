86a04636e65d49bb4368e2f2ceebab722f32b03c
Jack Hunt <jackmileshunt@gmail.com>
2024-06-16 22:20:17 +0100

    Update trainValSplit.m

--- data/trainValSplit.m
+++ data/trainValSplit.m
@@ -1,13 +1,17 @@
 function [x_train, y_train, x_val, y_val] = trainValSplit(x, y, train_percent)
-%TRAINVALSPLIT Summary of this function goes here
-%   Detailed explanation goes here
-    train_idx = 0;
-    val_idx = 0;
+%TRAINVALSPLIT Split data into training and validation sets.
+%   TODO make this indices only so shape need not be known.
+    n = size(x, 4);
+    idx = randperm(n);
+    val_start = floor(train_percent * n);
+
+    train_idx = idx(1:val_start-1);
+    val_idx = idx(val_start:end);
 
     x_train = x(:, :, :, train_idx);
-    y_train = y(train_idx);
+    y_train = y(train_idx, :);
 
     x_val = x(:, :, :, val_idx);
-    y_val = y(val_idx);
+    y_val = y(val_idx, :);
 end
 

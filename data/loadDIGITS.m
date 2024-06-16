0e20ead972f0f625e480166c0a9d57fc8661fefc
Jack Hunt <jackmileshunt@gmail.com>
2024-06-16 22:20:11 +0100

    Update loadDIGITS.m

--- data/loadDIGITS.m
+++ data/loadDIGITS.m
@@ -3,11 +3,11 @@ function digits = loadDIGITS()
     [x_train, y_train, ~] = digitTrain4DArrayData;
     [x_test, y_test, ~] = digitTest4DArrayData;
 
-    digits.x_train = x_train;
+    digits.x_train = x_train / 255;
     digits.y_train = onehotencode(categorical(y_train), 2);
     digits.n_train = size(digits.x_train, 4);
     
-    digits.x_test = x_test;
+    digits.x_test = x_test / 255;
     digits.y_test = onehotencode(categorical(y_test), 2);
     digits.n_test = size(digits.x_test, 4);
 end

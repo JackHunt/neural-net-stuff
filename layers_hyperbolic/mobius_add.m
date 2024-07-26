688727efe0499ca6fe0abb4ff35f60246b5160f3
Jack Hunt <jackmileshunt@gmail.com>
2024-07-26 17:52:05 +0100

    Sort out naming

--- layers_hyperbolic/mobius_add.m
+++ layers_hyperbolic/mobius_add.m
@@ -1,11 +1,11 @@
 function y = mobius_add(u, v, c)
 %MOBIUS_ADD Mobius addition.
     cuv = 1.0 + 2.0 * c * uv;
-    c_norm_v2 = c * norm(v)^2;
-    norm_u2 = norm(u)^2;
+    c_norm_v_sq = c * norm(v)^2;
+    norm_u_sq = norm(u)^2;
 
-    a = (cuv + c_norm_v2) * u + (1.0 - c * norm_u2) * v;
-    b = cuv + c * c_norm_v2 * norm_u2;
+    a = (cuv + c_norm_v_sq) * u + (1.0 - c * norm_u_sq) * v;
+    b = cuv + c * c_norm_v_sq * norm_u_sq;
 
     y = a / b;
 end

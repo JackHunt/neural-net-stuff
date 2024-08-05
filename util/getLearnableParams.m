4b09ffcfda8ea22704ed40840692992975f82f4d
Jack Hunt <jackmileshunt@gmail.com>
2024-08-05 23:42:52 +0100

    Delete getLearnableParams.m

--- util/getLearnableParams.m
+++
@@ -1,8 +0,0 @@
-function params = getLearnableParams(net)
-%GETLEARNABLEPARAMS Gets all the learnable params of a net as a list.
-    params = [];
-    for i = 1:numel(net.Learnables)
-        params = [params; net.Learnables.Value{i}(:)];
-    end
-end
-

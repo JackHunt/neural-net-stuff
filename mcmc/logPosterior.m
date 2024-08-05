83d71781df466df83d821daf33860cc9f49603b0
Jack Hunt <jackmileshunt@gmail.com>
2024-08-05 23:42:38 +0100

    Update logPosterior.m

--- mcmc/logPosterior.m
+++ mcmc/logPosterior.m
@@ -1,5 +1,8 @@
-function p = logPosterior(lik_fn, prior_fn)
-%LOGPOSTERIOR A generic log posterior - assumed to be zero arg lambdas.
-    p = lik_fn() + prior_fn();
+function p = logPosterior(params, net, X, Y, log_lik_fn, log_prior_fn)
+%LOGPOSTERIOR A generic log posterior
+    net = updateParameters(net, params);
+    ll = log_lik_fn(net, X, Y);
+    lp = log_prior_fn(params);
+    p = ll + lp;
 end
 

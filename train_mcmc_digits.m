495a4eb053db32f358a2ec9c5a9b9b62d4dc5c6c
Jack Hunt <jackmileshunt@gmail.com>
2024-08-05 23:42:43 +0100

    Update train_mcmc_digits.m

--- train_mcmc_digits.m
+++ train_mcmc_digits.m
@@ -4,6 +4,7 @@ clear variables;
 clc;
 
 addpath('data');
+addpath('mcmc')
 addpath('networks');
 addpath('layers');
 
@@ -13,9 +14,26 @@ train_split = 0.8;
 %% Load dataset & compute train/val split.
 digits = loadDIGITS();
 
-[x, y, x_val, y_val] = trainValSplit(digits.x_train, digits.y_train, train_split);
+[X, Y, X_val, Y_val] = trainValSplit(digits.x_train, digits.y_train, train_split);
 
 %% Setup network and training options.
+% Network architecture.
 layers = simpleDigitsClassifierDense();
-
 net = dlnetwork(layers);
+
+%% Setup log PDFs.
+% Likelihood & prior.
+log_lik = softmaxLogLik;
+log_prior = @(params) gaussLogPrior(params, 0, 1);
+
+% Posterior.
+log_posterior = @(params) logPosterior(params, net, X, Y, log_lik, log_prior);
+
+% Proposal distribution.
+proposal = @(params) params + 0.1 * randn(size(params));
+
+%% Run MCMC
+params_init = extractParameters(net);
+
+num_samples = 1000;
+samples = mhsample(params_init, num_samples, 'logpdf', log_posterior, 'proprnd', proposal);
